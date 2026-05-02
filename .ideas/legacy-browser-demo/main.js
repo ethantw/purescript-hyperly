90f3ed868222db2ffa6a43d633d2d0787762adb0
Chen Yijun <ethantw@me.com>
2026-05-02 18:52:15 +0800

    omit: unnecessary test files

--- .ideas/legacy-browser-demo/main.js
+++
@@ -1,6 +0,0 @@
-import { main } from '#output/Demo.Browser/index.js'
-// import { main } from '#output/Demo.ServerSide/index.js'
-import './main.css'
-
-main()
-console.log('`main()` in `Demo.Browser` was executed.')
